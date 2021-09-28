a546833a490f645ce7326bf7d419f2a3160ebbbc
Bruno Moreira <bruno_moreira.19@outlook.com>
2021-09-28 00:10:12 -0300

    manipulando a aplicação com context api e finalizando a aplicação

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from "react";
 import List from "./Pokemon/List/List";
 import View from "./Pokemon/View/View";
+import AppProvider from "./AppContext/Provider";
+import AppContext from "./AppContext/Context";
 import {
   BrowserRouter as Router,
   Switch,
@@ -13,18 +15,33 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <Router>
-        <h1>
-          <Link to="/pokemons/list">Brava pokelist</Link>
-        </h1>
-        <Switch>
-          <Route path="/pokemons/list" component={List} />
-          <Route path="/pokemons/:name" component={View} />
-          <Route path="/" exact>
-            <Redirect to="/pokemons/list" />
-          </Route>
-        </Switch>
-      </Router>
+      <AppProvider>
+        <Router>
+          <h1>
+            <Link to="/pokemons/list">Pokedex capenga</Link>
+          </h1>
+
+          <AppContext.Consumer>
+            {({ user }) => (
+              <p className="user">
+                <span> Nome: {user && user.name} </span>{" "}
+                <span>
+                  Total de pokemons visualizados:{" "}
+                  {Object.keys(user.pokedex).length}
+                </span>
+              </p>
+            )}
+          </AppContext.Consumer>
+
+          <Switch>
+            <Route path="/pokemons/list" component={List} />
+            <Route path="/pokemons/:name" component={View} />
+            <Route path="/" exact>
+              <Redirect to="/pokemons/list" />
+            </Route>
+          </Switch>
+        </Router>
+      </AppProvider>
     </div>
   );
 }
